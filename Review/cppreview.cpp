46bb5ae4e5fa1955dd7571468a1eddeda7cf0e3e
ariyonaty <ari.h.yonaty@gmail.com>
2020-08-26 22:24:59 -0700

    Quick test commit

---
+++ Review/cppreview.cpp
@@ -0,0 +1,30 @@
+/* 
+    Ari Yonaty
+    C++ Review
+ */
+
+// This is a single line comment
+/* 
+    This
+    is
+    a 
+    multiline
+    comment
+  */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <limits>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
