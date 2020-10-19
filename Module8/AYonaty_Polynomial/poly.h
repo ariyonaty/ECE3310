b51f1887f1b19c721ecdf83942ed74973ac5d55b
ariyonaty <ari.h.yonaty@gmail.com>
2020-10-18 21:09:43 -0700

    Done proj2.

---
+++ Module8/AYonaty_Polynomial/poly.h
@@ -0,0 +1,34 @@
+#ifndef POLY_H
+#define POLY_H
+
+#include "node.h"
+
+class Poly
+{
+private:
+    int count = { 0 };
+
+    Node* swap(Node* n1, Node* n2);
+    void sort();
+    void simplify();
+    bool isEmpty();
+
+public:
+    Poly();
+    ~Poly();
+
+    Node* phead = { nullptr };
+
+
+    Node* insert(int c, int e);
+    Node* insert(Node* pnode, int c, int e);
+
+    void deleteFront();
+    void deleteNode(Node** pnode);
+    void add(Poly poly1, Poly poly2);
+    void empty();
+    void display();
+    void printPoly();
+};
+
+#endif /* POLY_H */
