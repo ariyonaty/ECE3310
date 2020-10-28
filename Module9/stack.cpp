a5cfa96e2bea9b5ceae15e0e834e40e25167236a
ariyonaty <ari.h.yonaty@gmail.com>
2020-10-28 15:26:21 -0700

    linked list stack implementation with dec2bin application.

---
+++ Module9/stack.cpp
@@ -0,0 +1,153 @@
+/**
+ * Ari Yonaty
+ */
+
+#include <iostream>
+
+class Stack
+{
+private:
+    struct node
+    {
+        int data;
+        node *next;
+
+        node(int data, node *next = nullptr)
+        {
+            this->data = data;
+            this->next = next;
+        }
+    };
+
+public:
+    node *stk_top; // pointer to first node in stack
+    int stk_size;  // number of items in stack
+
+    Stack(/* args */);
+    ~Stack();
+
+    int size();
+    bool empty();
+    void clear();
+    int top();
+    void push(int data);
+    void pop();
+    void display();
+    void output();
+};
+
+Stack::Stack(/* args */)
+{
+    this->stk_top = nullptr;
+    this->stk_size = 0;
+}
+
+Stack::~Stack()
+{
+    clear();
+}
+
+bool Stack::empty()
+{
+    return (stk_top == nullptr);
+}
+
+void Stack::clear()
+{
+    while (stk_top)
+    {
+        pop();
+    }
+}
+
+void Stack::push(int data)
+{
+    stk_top = new node(data, stk_top);
+    stk_size++;
+}
+
+void Stack::pop()
+{
+    if (stk_top == nullptr)
+    {
+        std::cerr << "Stack is empty." << std::endl;
+        return;
+    }
+
+    node *tmp = stk_top;
+    stk_top = stk_top->next;
+    delete tmp;
+    stk_size--;
+}
+
+void Stack::display()
+{
+    node *ptr = stk_top;
+
+    std::cout << "*********** STACK ***********" << std::endl;
+
+    while (ptr)
+    {
+        std::cout << "[+] " << ptr->data << std::endl;
+        ptr = ptr->next;
+    }
+}
+
+void Stack::output()
+{
+    node *ptr = stk_top;
+    while (ptr)
+    {
+        std::cout << ptr->data;
+        ptr = ptr->next;
+    }
+    std::cout << std::endl;
+}
+
+int main(void)
+{
+    // node *p = nullptr;
+
+    // // push onto the front of the 'stack'
+    // p = new node(123, p);
+    // p = new node(456, p);
+    // p = new node(789, p);
+
+    // node *tmp = p;
+    // p = p->next;        // p now points to 456 node
+    // delete tmp;
+
+    Stack myStack = Stack();
+
+    // myStack.display();
+    // myStack.push(123);
+    // myStack.push(456);
+    // myStack.push(789);
+    // myStack.display();
+    // myStack.pop();
+    // myStack.display();
+    // myStack.push(789);
+    // myStack.display();
+    // myStack.clear();
+    // myStack.display();
+
+    int num, remainder;
+    std::cout << "*******************************" << std::endl;
+    std::cout << "****** Decimal to Binary ******" << std::endl;
+    std::cout << "*******************************" << std::endl;
+    std::cout << "Enter a decimal number: ";
+
+    std::cin >> num;
+
+    while (num > 0)
+    {
+        remainder = num % 2;
+        num = num / 2;
+        myStack.push(remainder);
+    }
+
+    std::cout << "Binary Equivalent: ";
+    myStack.output();
+
+    return 0;
+}
